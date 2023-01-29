a1a6c7ec5201142905560dfe25abf5a917c8c3c0
Martin <martinsko@sfr.fr>
2023-01-28 19:31:06 -0500

    add ligth alert

---
+++ Assets/Scripts/Manager/AlertLightManager.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AlertLightManager : MonoBehaviour
+{
+
+     Light myLight;
+    // Start is called before the first frame update
+    void Start()
+    {
+        myLight = GetComponent<Light>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        myLight.intensity = Mathf.PingPong(Time.time, 2);
+    }
+}
