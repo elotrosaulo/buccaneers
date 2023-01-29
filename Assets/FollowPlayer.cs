8f0979033ee0b8d0ad6b9013c863ca580b23caec
DESKTOP-DVR21EE\Mishael <50791043+megaiden@users.noreply.github.com>
2023-01-29 16:58:42 -0500

    enemy sprite working!

--- Assets/FollowPlayer.cs
+++ Assets/FollowPlayer.cs
@@ -6,7 +6,8 @@ using UnityEngine.AI;
 public class FollowPlayer : MonoBehaviour
 {
     public Transform target;
-
+    public Rigidbody enemySpriteRigidBody;
+    public Transform enemySpritetransform;
     private NavMeshAgent nav;
     private void OnEnable()
     {
@@ -23,7 +24,14 @@ public class FollowPlayer : MonoBehaviour
         nav = GetComponent<NavMeshAgent>();
     }
 
-
+    private void FixedUpdate()
+    {
+        var _moveVector = new Vector3(transform.position.x,enemySpritetransform.position.y,transform.position.z) ;
+        
+        //transform.parent.position = transform.position;
+        enemySpriteRigidBody.MovePosition(_moveVector);
+        
+    }
     void Update()
     {
         nav.SetDestination(target.position);
