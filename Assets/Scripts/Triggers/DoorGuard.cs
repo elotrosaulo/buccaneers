bfaf03a601bc50d6d1bed3180e30f3a366fec8bd
Martin <martinsko@sfr.fr>
2023-02-03 16:21:47 -0500

    Fix Dialogue Box

--- Assets/Scripts/Triggers/DoorGuard.cs
+++ Assets/Scripts/Triggers/DoorGuard.cs
@@ -3,12 +3,23 @@ using UnityEngine;
 public class DoorGuard : MonoBehaviour
 {
     public Dialogue dialogue;
+    public Canvas Ui;
+
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Player"))
         {
+            Ui.enabled = false ; 
+            
             FindObjectOfType<DialogueManager>().StartDialogue(dialogue, true, "Try Your Luck", DialogueManager.ActionType.OpenMiniGame);
+        }
+    }
+    private void OnTriggerExit(Collider other){
+        if (other.gameObject.CompareTag("Player"))
+        {
+            FindObjectOfType<DialogueManager>().EndDialogue();
+            Ui.enabled = true ; 
         }
     }
 }
